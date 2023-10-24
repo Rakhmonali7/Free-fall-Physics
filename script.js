d2be4477cc22197be09ec516c058bc07ae74546a
Rakhmonali7 <zamonalievr7@gmail.com>
2023-10-24 22:39:25 +0900

    catching errors

--- script.js
+++ script.js
@@ -2,190 +2,10 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-<<<<<<< HEAD
-const userInput = document.getElementById('userInput')
-const buttn = document.getElementById('btn')
-let countryName
 
-// ///////////////////////////////////////   with user interface
-// function getCountryData(countryName){
-// const request = new XMLHttpRequest()
-// request.open('GET', `https://countries-api-836d.onrender.com/countries/name/${countryName}`)
-// request.send()
-
-//     request.addEventListener('load', function(){
-//         const [data]= JSON.parse(this.responseText)
-//         console.log(data);
-
-//         countriesContainer.innerHTML = '';
-
-//         const html = `
-=======
 const userInput = document.getElementById('userInput');
 const buttn = document.getElementById('btn');
 let countryName;
-
-///////////////////////////////////////
-// function getCountryData(countryName) {
-//   const request = new XMLHttpRequest();
-//   request.open(
-//     'GET',
-//     `https://countries-api-836d.onrender.com/countries/name/${countryName}`
-//   );
-//   request.send();
-
-//   request.addEventListener('load', function () {
-//     const [data] = JSON.parse(this.responseText);
-//     console.log(data);
-
-//     countriesContainer.innerHTML = '';
-
-//     const html = `
->>>>>>> 6f8e763 (chaining)
-//             <article class="country">
-//               <img class="country__img" src="${data.flag}" />
-//               <div class="country__data">
-//                 <h3 class="country__name">${data.name}</h3>
-//                 <h4 class="country__region">${data.region}</h4>
-<<<<<<< HEAD
-//                 <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} million</p>
-//                 <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//                 <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-//               </div>
-//             </article>
-//         `
-//         countriesContainer.insertAdjacentHTML('beforeend', html)
-//         countriesContainer.style.opacity = 1;
-//     })
-
-// }
-// buttn.addEventListener('click', function(e){
-//     e.preventDefault()
-//     countryName = userInput.value
-//     getCountryData(countryName)
-// })
-
-
-// ///////////////////////////////////////////////
-//2222222222222222222222222222222222222222222222222222222
- 
-
-// function getCountryAndNeighbour(name) {
-//   const request = new XMLHttpRequest()
-//   request.open('GET', `https://countries-api-836d.onrender.com/countries/name/${name}`)
-
-//   request.send()
-
-//   request.addEventListener('load', function () {
-//     const [data] = JSON.parse(this.responseText)
-//     const neighbour = data.borders ?. [0]
-
-//     //render country1
-
-//     renderCountry(data)
-
-//     //AJAX call country 2
-
-//     const request2 = new XMLHttpRequest()
-//     request2.open('GET', `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`)
-//     request2.send()
-
-//     request2.addEventListener('load', function () {
-//       const data2 = JSON.parse(this.responseText)
-//       renderCountry(data2, 'neighbour')
-//     })
-//   })
-// }
-// getCountryAndNeighbour('usa')
-const renderCountry = function(data, namecl=''){
-  const html = `
-             <article class="country ${namecl}" >
-               <img class="country__img" src="${data.flag}" />
-               <div class="country__data">
-                 <h3 class="country__name">${data.name}</h3>
-                 <h4 class="country__region">${data.region}</h4>
-                 <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} million</p>
-                 <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-                 <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-               </div>
-             </article>
-         `
-         countriesContainer.insertAdjacentHTML('beforeend', html)
-         countriesContainer.style.opacity = 1;
-     }
-
-//////////////3333333333333333333333333333//////////////
-
-
-const getCountryData = function(country){
-  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
-  .then(function (response) {
-   return response.json()
-  }).then(function(data){
-      renderCountry(data[0])
-      const neighbour = data[0] ?. borders[0]
-      return fetch(`https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`)
-      
-      }).then((back)=> back.json())
-      .then(function(data){
-        renderCountry(data, 'neighbour')
-  })
-}
-getCountryData('portugal')
-=======
-//                 <p class="country__row"><span>👫</span>${(
-//                   +data.population / 1000000
-//                 ).toFixed(1)} million</p>
-//                 <p class="country__row"><span>🗣️</span>${
-//                   data.languages[0].name
-//                 }</p>
-//                 <p class="country__row"><span>💰</span>${
-//                   data.currencies[0].name
-//                 }</p>
-//               </div>
-//             </article>
-//         `;
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-//     countriesContainer.style.opacity = 1;
-//   });
-// }
-
-// const renderCountry = function (data) {
-//   const html = `
-//             <article class="country">
-//               <img class="country__img" src="${data.flag}" />
-//               <div class="country__data">
-//                 <h3 class="country__name">${data.name}</h3>
-//                 <h4 class="country__region">${data.region}</h4>
-//                 <p class="country__row"><span>👫</span>${(
-//                   +data.population / 1000000
-//                 ).toFixed(1)} million</p>
-//                 <p class="country__row"><span>🗣️</span>${
-//                   data.languages[0].name
-//                 }</p>
-//                 <p class="country__row"><span>💰</span>${
-//                   data.currencies[0].name
-//                 }</p>
-//               </div>
-//             </article>
-//         `;
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = 1;
-// };
-
-// const getCountryData = function (country) {
-//   //
-//   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
-//     .then(response => response.json())
-//     .then(function (data) {
-//       const [newData] = data;
-//       renderCountry(newData);
-//       fetch(
-//         `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
-//       );
-//     });
-// };
-// getCountryData('portugal');
 
 const renderCountry = function (data, neighbour = '') {
   const html = `
@@ -207,8 +27,11 @@ const renderCountry = function (data, neighbour = '') {
             </article>
         `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
 };
+
+function renderError(msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+}
 
 const getCountryData = function (countryName) {
   // country 1
@@ -226,16 +49,18 @@ const getCountryData = function (countryName) {
       )
         .then(response => response.json())
         .then(data => renderCountry(data, 'neighbour'));
-    });
+    })
+    .catch(err =>
+      renderError(`UPS something went wrong ${err.message}, Try Again!`)
+    )
+    .finally(() => (countriesContainer.style.opacity = 1));
 };
-<<<<<<< HEAD
-getCountryData('portugal');
->>>>>>> 6f8e763 (chaining)
-=======
 
-buttn.addEventListener('click', function (e) {
-  e.preventDefault();
-  countryName = userInput.value;
-  getCountryData(countryName);
+// buttn.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   countryName = userInput.value;
+//   getCountryData(countryName);
+// });
+btn.addEventListener('click', function () {
+  getCountryData('portugal');
 });
->>>>>>> 9282d20 (chaini part2)
